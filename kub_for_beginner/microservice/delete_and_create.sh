1febffb2cfb4f18f27e89416fddb8a048e14482b
Alok Ranjan Joshi <alokranjanjoshi07567@gmail.com>
2026-04-29 00:49:27 +0530

    again created

--- kub_for_beginner/microservice/delete_and_create.sh
+++ kub_for_beginner/microservice/delete_and_create.sh
@@ -1,6 +1,20 @@
 kubectl get deployment | grep -vi ready | awk -F ' ' '{print $1}' | xargs kubectl delete deployment
 kubectl get service | grep -vi 'external-ip' | awk -F ' ' '{print $1}' | xargs kubectl delete service
 
+# kubectl apply -f ./temp/kube_deployment_redis.yaml
+# kubectl apply -f ./temp/kube_service_redis.yaml
+
+# kubectl apply -f ./temp/kube_deployment_postgres.yaml
+# kubectl apply -f ./temp/kube_service_postgres.yaml
+
+# kubectl apply -f ./temp/kube_deployment_vote_app.yaml
+# kubectl apply -f ./temp/kube_service_vote.yaml
+
+# kubectl apply -f ./temp/kube_deployment_result_app.yaml
+# kubectl apply -f ./temp/kube_service_result.yaml
+
+# kubectl apply -f ./temp/kube_deployment_worker.yaml
+
 kubectl apply -f kube_deployment_redis.yaml
 kubectl apply -f kube_service_redis.yaml
 
